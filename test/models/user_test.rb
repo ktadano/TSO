f19d49f1676331b817d18855283f793e7e195d9e
ktadano <ktadano@ktadano-iMac.local>
2016-06-22 18:47:32 +0900

    test valid user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: "Test User")
+  end
+
+  test "should be valid" do
+    assert_not @user.valid?
+  end
 end
