4a63755692b9702a2c18e1eabc0f9ed828a01cfc
ktadano <ktadano@ktadano-iMac.local>
2016-06-22 18:06:37 +0900

    test invalid signup information

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "invalid user resistration" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path,
+        user: {
+          name:  "",
+          password: "inv",
+          password_confirmation: "alid"
+        }
+    end
+    assert_template 'users/new'
+  end
 end
