53b0b597068666dbab3bc515f5b5a261f0c4e662
ktadano <ktadano@ktadano-iMac.local>
2016-06-22 11:06:09 +0900

    add users resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'signup' => 'users#new'
 
+  resources :users, except: [:index]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
