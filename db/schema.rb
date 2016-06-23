0b47577be75ba04eb324786b566311f306ba8153
ktadano <ktadano@ktadano-iMac.local>
2016-06-23 18:47:16 +0900

    add user migrate password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,9 @@ ActiveRecord::Schema.define(version: 20160622062454) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "password_digest"
   end
 
 end
