a4fa7329e5b402d2bf1f90efea08b8e897de5845
ktadano <ktadano@ktadano-iMac.local>
2016-06-23 12:21:27 +0900

    user login to show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,12 @@
 class SessionsController < ApplicationController
-  def new
+
+  def create
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      render 'new'
+    end
   end
 end
