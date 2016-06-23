e06f367946adab54296a4b510092965c828d896e
ktadano <ktadano@ktadano-iMac.local>
2016-06-23 11:15:29 +0900

    generate session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'signup' => 'users#new'
-
-  resources :users, only: [:new, :create, :show]
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  resources :users, only: [:new, :show, :create]
 end
