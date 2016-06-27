84c4de9824764be566fb36b520c8b5631498903c
ktadano <ktadano@ktadano-iMac.local>
2016-06-27 15:37:02 +0900

    user has_many products

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: {maximum: 32}
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
+  has_many :products
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ?
