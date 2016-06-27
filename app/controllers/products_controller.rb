7cc7ae7e78053865e0ef136e6a62b4a895bec7fc
ktadano <ktadano@ktadano-iMac.local>
2016-06-27 17:34:29 +0900

    create product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,5 +8,20 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = current_user.products.build(product_params)
+    if @product.save
+      flash[:success] = "商品が登録されました！"
+      redirect_to @product
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name,
+                                    :current_bit,
+                                    :time_left)
   end
 end
