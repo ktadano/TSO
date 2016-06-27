5acf825cac7e9bc368cba8b8b845a099bec14424
ktadano <ktadano@ktadano-iMac.local>
2016-06-27 15:55:40 +0900

    add validates product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :user
+  validates :user_id, presence: true
+  validates :name, presence: true, length: { maximum: 64 }
+  validates :current_bit, presence: true
+  validates :time_left, presence: true
 end
