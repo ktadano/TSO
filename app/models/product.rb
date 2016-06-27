d257bb4b5a934577f700ac40d187c38351d3e420
ktadano <ktadano@ktadano-iMac.local>
2016-06-27 19:39:13 +0900

    display product information

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
   validates :name, presence: true, length: { maximum: 64 }
-  validates :current_bit, presence: true
+  validates :current_bid, presence: true, length: { maximum: 8 }
+  validates :discription, presence: true, length: { maximum: 128 }
   validates :time_left, presence: true
 end
