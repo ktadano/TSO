511a6ac9802d19b2b5362cb76ade322d770c2bd1
ktadano <ktadano@ktadano-iMac.local>
2016-06-30 15:50:33 +0900

    add migration bid

--- db/migrate/20160630063335_create_bids.rb
+++ db/migrate/20160630063335_create_bids.rb
@@ -1,6 +1,9 @@
 class CreateBids < ActiveRecord::Migration
   def change
     create_table :bids do |t|
+      t.integer :user_id
+      t.integer :product_id
+      t.integer :current_bid
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627025530) do
+ActiveRecord::Schema.define(version: 20160630063335) do
+
+  create_table "bids", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "product_id"
+    t.integer  "current_bid"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
