6e1463f1b9af4f1fe8c0edddd05c619d1dd64a5f
ktadano <ktadano@ktadano-iMac.local>
2016-07-05 10:42:54 +0900

    show bid product list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @products = @user.products.paginate(page: params[:page])
+    @bid_items = my_bid_list
   end
 
   def new
@@ -23,5 +24,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+  def my_bid_list
+    bid_ids = Bid.where("user_id = ?", @user).
+      select(:product_id).
+      uniq
+    return Product.where(id: bid_ids.pluck(:product_id))
   end
 end
